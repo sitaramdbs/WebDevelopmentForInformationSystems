286fdcdeff0b02c0abf8aa9a97b165aa360b65e0
sitaramdbs <129968031+sitaramdbs@users.noreply.github.com>
2023-04-06 16:09:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
-
+app.config['DATABASE_URL'] = f'postgres://gxudqwloqcjtvc:5cf1d1d4c7754c27092e0632145ce2b2527380033b1af55a0e0ade54082b6c93@ec2-52-215-68-14.eu-west-1.compute.amazonaws.com:5432/d4917i2g328329'
 # Creates a connection to the database
 db = SQL ( "sqlite:///data.db" )
 
